643589d9b3c8dc22532fa157fdde6b3332bb16f2
Lyk98 <li.yi.kai1998@qq.com>
2018-04-27 16:32:48 +0800

    Test Build

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -1,3 +1,5 @@
+package Test;
+
 import AST_Node.ProgNode;
 import Frontend.ASTBuilder;
 import Parser.LMxLexer;
@@ -5,18 +7,15 @@ import Parser.LMxParser;
 import Tools.ASTViewer;
 import Tools.SymbolTable;
 import org.antlr.v4.runtime.CharStream;
-import org.antlr.v4.runtime.CharStreams;
 import org.antlr.v4.runtime.CommonTokenStream;
 import org.antlr.v4.runtime.tree.ParseTree;
 
-import java.io.InputStream;
-
 import static org.antlr.v4.runtime.CharStreams.fromFileName;
 
-public class Main {
+public class Test {
     public static void main(String[] args) throws Exception {
-        InputStream inS = System.in;
-        CharStream stream = CharStreams.fromStream(inS);
+        String inputFile = "D:\\LYK\\Class\\Compiler\\LMx_Compiler\\Compiler\\src\\Test\\code.txt";
+        CharStream stream = fromFileName(inputFile);
         LMxLexer lexer              = new LMxLexer(stream);
         CommonTokenStream tokens    = new CommonTokenStream(lexer);
         LMxParser parser            = new LMxParser(tokens);
